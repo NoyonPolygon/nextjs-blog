1e69f1b8f487a9a0d50cdc634c2d6c229dd0b7c9
NoyonPolygon <nuruzzaman@polygontech.xyz>
2023-10-03 16:31:23 +0600

    remove nextjs links

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi there, I am a enthusiastic software engineer who loves to solve real world problems.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
